ec27016f1d48a52afc12e42e094c8e48632bd8f4
Henrik Jonsson <me@hkjn.me>
2014-08-02 19:09:15 +0200

    adds secret folders to --exclude

--- backup_zero_unity.sh
+++ backup_zero_unity.sh
@@ -5,7 +5,7 @@
 # Note that files not readable by current user (notably /root,
 # including /root/keys) will not be copied.
 BACKUP_DIR=/media/musashi/backup/zero-unity-backup-$(date +%Y-%m-%d)
-rsync -vaHAX /* ${BACKUP_DIR} --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/meta/*,/lost+found}
+rsync -vaHAX /* ${BACKUP_DIR} --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/meta/*,/home/${USER}/media/*,/home/${USER}/.cache/*,/home/${USER}/.secret/media/*,/home/${USER}/secret/media/*,/lost+found}
 
 # Restore with the reverse command:
 # rsync -vaHAX ${BACKUP_DIR} /* --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,meta/*,/lost+found}
