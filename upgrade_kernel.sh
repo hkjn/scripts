d26e45a1719ad5719bc27f6c6595724c368ba642
Henrik Jonsson <me@hkjn.me>
2015-10-06 12:55:00 +0200

    Make upgrade_kernel.sh slightly less bad

--- upgrade_kernel.sh
+++ upgrade_kernel.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+#
+# Back up current kernel and upgrade to new one.
+#
 
-set -e
+set -ueo pipefail
 
-# NOTE: Not tested yet.
-
-# Backs up current kernel and upgrades to new one.
-# This script requires root permissions.
-
-LIBS=$(ls -d /lib/modules/?.??.?-?-ARCH | tail -n 1)
+LIBS=$(ls -d /lib/modules/?.*-ARCH | tail -n 1)
 VERSION=$(echo $LIBS | cut -d '/' -f4)
 BACKUP=/root/kernel-backups/${VERSION}-$(date +%Y%m%d)
 BOOT=/boot/EFI/arch
@@ -19,6 +17,8 @@ KERNEL=${BOOT}/vmlinuz-arch-crypto
 
 echo "Backing up previous kernel modules ${LIBS} to ${BACKUP}/ and ${LIBS}-stable/.."
 sudo cp -r ${LIBS} ${BACKUP}/
+# We create a copy with a different name, since the upgrade writes
+# over the directory otherwise.
 sudo cp -r ${LIBS} ${LIBS}-stable/
 
 echo "Backing up previous compiled kernel to ${BACKUP}/${KERNEL}.efi and rotating it to ${KERNEL}-stable.efi.."
@@ -32,7 +32,8 @@ sudo cp -v ${BOOT}/initramfs-arch-crypto.img ${BOOT}/initramfs-arch-crypto-stabl
 sudo cp -v ${BOOT}/initramfs-arch-crypto-fallback.img ${BACKUP}/
 sudo cp -v ${BOOT}/initramfs-arch-crypto-fallback.img ${BOOT}/initramfs-arch-crypto-fallback-stable.img
 
-# At this point we can upgrade. (Upgrading also runs mkinitcpio -p linux; regenerating /boot/initramfs-linux{-fallback}.img).
+# At this point we can upgrade.  Upgrading the kernel runs `mkinitcpio
+# -p linux`, which regenerates /boot/initramfs-linux{-fallback}.img).
 echo 'Upgrading system..'
 sudo pacman -Syyu
 
