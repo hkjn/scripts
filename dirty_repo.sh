9488cd57e54fe1213f91d4d4746f9b3b4c3fa4cc
Henrik Jonsson <me@hkjn.me>
2016-04-09 14:47:35 +0200

    Remove commeted out code

--- dirty_repo.sh
+++ dirty_repo.sh
@@ -5,16 +5,18 @@
 # 2. With git repos inside
 # 3. With a clean working tree
 #
+# TODO(hkjn): Reimplement git-wtf.rb calls in lib/ bash scripts, to
+# avoid wrapping it in shell call here (and having dependency on
+# ruby).
 # TODO(hkjn): Also look for LICENSE, README.md?
 # TODO(hkjn): Also check if everything is pushed up: git log origin/master..master
 #
-set -euo pipefail
 
 BASENAME="hkjn.me"
 LIBDIR="lib"
 
 load() {
-	set -euo pipefail
+	set -uo pipefail
 	local p="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	while [ $(basename $p) != "$BASENAME" ] && [ $p != "/" ]; do p="${p/\/$(basename $p)/}"; done
 	source "$p/$LIBDIR/$1" || { echo "FATAL: Couldn't load $p/$LIBDIR/$1." >&2; exit 1; }
@@ -26,7 +28,7 @@ check() {
 	cd "$BASE"
 	local dirty=0
 	for d in $(ls); do
-		if [ ! -d "$d" ]; then
+	if [ ! -d "$d" ]; then
 			fatal "Not a directory: '$d'"
 		fi
 		cd "$d"
@@ -34,20 +36,13 @@ check() {
 			fatal "Not a git repo: '$d/.git' doesn't exist"
 		fi
 
-		info="$(git-wtf.rb)"
-		if $? -ne 0; then
-			error "$info"
+		if ! msg="$(git-wtf.rb 2>&1)"; then
+			error "Dirty tree in '$d' repo:\n'$msg'"
 		fi
-#		local gs="$(git status)"
-#		if ! echo "$gs" | grep 'nothing to commit, working directory clean'; then
-#			error "Dirty tree in '$d' repo:"
-#			error "$gs"
-#			dirty=1
-#		fi
 		cd ..
 	done
 	return $dirty
 }
-
-which git-wtf.rb 1>/dev/null || fatal "No git-wtf.rb found on PATH."
+which ruby 1>/dev/null || fatal "No 'ruby' found on PATH."
+which git-wtf.rb 1>/dev/null || fatal "No 'git-wtf.rb' found on PATH."
 check
