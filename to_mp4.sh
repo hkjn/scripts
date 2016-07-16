64c81e4614f0e322051682807730c7883be6dbf8
Henrik Jonsson <me@hkjn.me>
2016-07-16 21:36:21 +0200

    Also handle re-encoding video

--- to_mp4.sh
+++ to_mp4.sh
@@ -5,7 +5,8 @@
 set -euo pipefail
 for v in *.mkv; do
   ffmpeg -i "$v" -vcodec copy -acodec copy "${v%.mkv}.mp4" ||
-  ffmpeg -i "$v" -vcodec copy -c:a aac "${v%.mkv}.mp4"
+  ffmpeg -i "$v" -vcodec copy -c:a aac "${v%.mkv}.mp4" ||
+  ffmpeg -i "$v" -c:v libx264 -acodec copy "${v%.mkv}.mp4"
 done
 for  v in *.webm; do
   ffmpeg -i "$v" -vcodec libx264 "${v%.webm}.mp4"
