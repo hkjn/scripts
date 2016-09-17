4962f96638ec14ed4f5970b0fa29c1b718f0c912
Henrik Jonsson <me@hkjn.me>
2016-09-17 13:34:17 +0200

    Improve bootstrap_host.sh

--- bootstrap_host.sh
+++ bootstrap_host.sh
@@ -3,24 +3,39 @@
 # Runs minimal steps to bootstrap a new host to a good state.
 #
 
-# TODO(hkjn): Set shell in /etc/passwd or use adduser.
-useradd -G sudo,docker zero
-mkdir -p /home/zero/.ssh
-mv *.pub /home/zero/.ssh/authorized_keys
-chown -R zero:zero /home/zero/
-chmod 700 /home/zero/.ssh
-chmod 400 /home/zero/.ssh/authorized_keys
+set -euo pipefail
+
+die() {
+  echo "FATAL: $@" >&2
+  exit 1
+}
+
+[[ $UID -eq 0 ]] || die 'Needs to be root on remote host to bootstrap.'
+
+RUSER=${RUSER:-"zero"}
+
+useradd -G sudo,docker -s /bin/bash $RUSER
+mkdir -p /home/$RUSER/.ssh
+cp .ssh/authorized_keys /home/$RUSER/.ssh/
+chown -R $RUSER:$RUSER /home/$RUSER/
+chmod 700 /home/$RUSER/.ssh
+chmod 400 /home/$RUSER/.ssh/authorized_keys
 sed -e s/22/6200/ \
 		-e s/'PermitRootLogin without-password'/'PermitRootLogin no'/ \
 		-i /etc/ssh/sshd_config
 systemctl restart sshd
-passwd zero
-su - zero
+passwd -d $RUSER
+echo "$RUSER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user_sudo
+chmod 0440 /etc/sudoers.d/user_sudo
+
+su - $RUSER
 mkdir -p src/hkjn.me
 cd src/hkjn.me
 git clone https://github.com/hkjn/scripts.git
 git clone https://github.com/hkjn/dotfiles.git
-cd scripts
+cd dotfiles
 cp .bash* ~/
+
+echo 'Done bootstrapping host.'
 
 
