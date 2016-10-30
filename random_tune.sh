88529f6d77f9bf7cd302b9d71b23378c4eb02195
Henrik Jonsson <me@hkjn.me>
2016-10-30 06:41:03 +0100

    Fix random_tune.sh

--- random_tune.sh
+++ random_tune.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+set -euo pipefail
 # Play some random tunes.
-MUSIC_DIR=/home/$USER/media/music/
-mplayer "$(ls ${MUSIC_DIR}*.mp3 | shuf -n1)"
+MUSIC_DIR=${MUSIC_DIR:-/home/$USER/media/music/}
+[[ -e $MUSIC_DIR ]] || {
+  echo "No such MUSIC_DIR='$MUSIC_DIR'" >&2
+  exit 1
+}
+mplayer "$(find $MUSIC_DIR -name '*.mp3' | shuf -n1)"
