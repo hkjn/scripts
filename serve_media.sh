bbd8fe6fd6c3630f596e3ecea6a648ece167f7f2
Henrik Jonsson <me@hkjn.me>
2016-11-14 12:30:01 +0100

    Update paths

--- serve_media.sh
+++ serve_media.sh
@@ -16,7 +16,8 @@ start staging $HOME/staging 8081
 start media $HOME/media 8082
 start tvideo /media/timothy/video 8083
 start tcomedic /media/timothy/comedic 8084
-start usb1 /run/media/zero/USB20FD 8085
-start usb2 /run/media/zero/f538fa97-80d3-4ef3-9010-99460637a69a 8086
+start teducational /media/timothy/educational 8085
+#start usb1 /run/media/zero/USB20FD 8085
+#start usb2 /run/media/zero/f538fa97-80d3-4ef3-9010-99460637a69a 8086
 
 echo "Started media fileserver containers."
