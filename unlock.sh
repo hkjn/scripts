93184cbf861d7b314327ce5b3217796ca290733f
Henrik Jonsson <me@hkjn.me>
2017-01-22 09:24:15 +0100

    Make unlock.sh handle re-encryption, but only if necessary
    Open nano with plaintext file, which now is stored under tempfs,, and
    removed securely in EXIT handler if at all possible.

--- unlock.sh
+++ unlock.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
-# Decrypts the ciphertext file (without .pgp) given as first argument.
+# Unlocks encrypted file given as first argument for viewing/editing.
+#
+# If the contents of the file changed, the cleartext file is re-encrypted.
+#
+# Regardless, the plaintext file is securely removed as the editor is
+# closed, and is stored on tempfs only in the meanwhile.
 #
 load() {
 	local ns="hkjn.me"
@@ -16,17 +21,44 @@ load "logging.sh"
 
 BASE="$GOPATH/src/bitbucket.org/hkjn/passwords"
 CRYPT="$BASE/$1"
-if [[ ! -e "$CRYPT" ]]; then
+RECIPIENT="me@hkjn.me"
+[[ -e "$CRYPT" ]] || {
   info "No such file '$CRYPT', trying $CRYPT.pgp.."
   CRYPT="$CRYPT.pgp"
-fi
-if [[ ! -e "$CRYPT" ]]; then
+}
+[[ -e "$CRYPT" ]] || {
   info "No such file '$CRYPT'"
   fatal "No such file '$CRYPT' or '$CRYPT.pgp'"
-fi
-CLEAR="$BASE/clear/${1%.pgp}"
+}
 
-mkdir -p clear/
-echo "Decrypting $CRYPT -> $CLEAR"
-gpg --output $CLEAR --decrypt $CRYPT
+cleanup() {
+  if which srm 1>/dev/null; then
+    srm -fvi ${CLEAR}*
+  elif which shred 1>/dev/null; then
+    shred ${CLEAR}*
+    rm -vrf ${CLEAR}*
+  else
+    echo "Neither 'srm' or 'shred' was installed; can't remove '${CLEAR}' securely."
+    rm -vrf ${CLEAR}*
+  fi
+  info "Dropping GPG identities from agent.."
+  echo RELOADAGENT | gpg-connect-agent
+}
+
+CLEAR="$(mktemp)"
+trap cleanup EXIT
+
+info "Decrypting $CRYPT -> $CLEAR"
+gpg --batch --yes --output $CLEAR --decrypt $CRYPT
 chmod 600 $CLEAR
+
+CHECKSUM_BEFORE=$(sha256sum $CLEAR)
+nano $CLEAR
+CHECKSUM_AFTER=$(sha256sum $CLEAR)
+
+if [[ $CHECKSUM_BEFORE != $CHECKSUM_AFTER ]]; then
+  info "Contents changed, re-encrypting ${CLEAR} -> $CRYPT"
+  gpg --batch --yes --output $CRYPT --encrypt --armor --recipient $RECIPIENT $CLEAR
+fi
+
+info "All done."
