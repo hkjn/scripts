c135dcb3050a96fc158fdda2104a06ea021e0a28
Henrik Jonsson <me@hkjn.me>
2017-03-12 15:40:38 +0100

    Consistently use spaces

--- go_git_hooks.sh
+++ go_git_hooks.sh
@@ -1,57 +1,58 @@
 # Golang git hooks, useful in constructing e.g. pre-commit, pre-push.
 
-function has_conflicts() {
-		echo "Checking for merge conflicts.." >&2
-		conflicts=$(git diff --cached --name-only -S'<<<<<< HEAD')
-		if [ -n "$conflicts" ]; then
-				echo "Unresolved merge conflicts in this commit:" >&2
-				echo $conflicts >&2
-				return 1
-		fi
-		return 0
+has_conflicts() {
+  echo "Checking for merge conflicts.." >&2
+  conflicts=$(git diff --cached --name-only -S'<<<<<< HEAD')
+  if [[ -n "$conflicts" ]]; then
+    echo "Unresolved merge conflicts in this commit:" >&2
+    echo $conflicts >&2
+    return 1
+  fi
+  return 0
 }
 
-function needs_gofmt() {
-	echo "Checking if any files need gofmt.." >&2
-	IFS=$'\n'
-	if [ git rev-parse HEAD >/dev/null 2>&1 ]; then
-		FILES=$(git diff --cached --name-only | grep -e '\.go$');
-	else
-		FILES=$(git ls-files -c | grep -e '\.go$');
-	fi
-	failed=0
-	for file in $FILES; do
-		if [ ! -e "$file" ]; then
-			# If a cached file doesn't exist it must be about to be deleted
-			# in this commit, so no need to check it.
-			continue
-		fi
-		badfile="$(gofmt -l "$file")"
-		if test -n "$badfile" ; then
-			echo "git pre-commit check failed: file needs gofmt: $file" >&2
-			failed=1
-		fi
-	done
-	if [ $failed -ne 0 ]; then
-		return 1
-	fi
-	return 0
+needs_gofmt() {
+  echo "Checking if any files need gofmt.." >&2
+  IFS=$'\n'
+  local files=""
+  if [ git rev-parse HEAD >/dev/null 2>&1 ]; then
+    files=$(git diff --cached --name-only | grep -e '\.go$');
+  else
+    files=$(git ls-files -c | grep -e '\.go$');
+  fi
+  local failed=0
+  for file in $files; do
+    if [[ ! -e "$file" ]]; then
+      # If a cached file doesn't exist it must be about to be deleted
+      # in this commit, so no need to check it.
+      continue
+    fi
+    local badfile="$(gofmt -l "$file")"
+    if test -n "$badfile" ; then
+      echo "git pre-commit check failed: file needs gofmt: $file" >&2
+      failed=1
+    fi
+  done
+  if [[ $failed -ne 0 ]]; then
+    return 1
+  fi
+  return 0
 }
 
-function prevent_dirty_tree() {
-	if [ "$#" -ne 2 ]; then
-		echo "Usage: prevent_dirty_tree [directory] [comment]" >&2
-		return 1
-	fi
-	if [ $(git status --porcelain 2>/dev/null ${1} | grep "^ M" | wc -l) -ne "0" ]; then
-		echo "Diff in ${1} - ${2}:" >&2
-		echo $(git diff --numstat ${1})
-		return 1
-	fi
-	return 0
+prevent_dirty_tree() {
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: prevent_dirty_tree [directory] [comment]" >&2
+    return 1
+  fi
+  if [ $(git status --porcelain 2>/dev/null ${1} | grep "^ M" | wc -l) -ne "0" ]; then
+    echo "Diff in ${1} - ${2}:" >&2
+    echo $(git diff --numstat ${1})
+    return 1
+  fi
+  return 0
 }
 
-function prevent_hacks() {
+prevent_hacks() {
   echo "Checking for strings indicating hacks.." >&2
   local files=""
   if [ git rev-parse HEAD >/dev/null 2>&1 ]; then
@@ -87,7 +88,7 @@ function prevent_hacks() {
   return 0
 }
 
-function run_go_tests() {
+run_go_tests() {
   echo "Running all Go tests.." >&2
   targets=$(go list ./... 2>/dev/null | grep -v /vendor/)
   if [[ ! "$targets" ]]; then
@@ -107,43 +108,44 @@ function run_go_tests() {
   return 1
 }
 
-function run_go_vet() {
-	echo "Running Go vet command.." >&2
-	local targets=$(go list ./... 2>/dev/null | grep -v /vendor/)
-	if [[ ! "$targets" ]]; then
-		return 0 # Nothing to test
-	fi
-	output=$(go vet $targets 2>&1)
-	if [ $? -eq 0 ]; then
-		return 0
-	fi
-	if echo "$output" | grep "matched no packages" >/dev/null; then
-		# Special case for "there's no packages in this repo", which is fine.
-		return 0
-	fi
-	echo "Go vet failed:\n$output" >&2
-	return 1
+run_go_vet() {
+  echo "Running Go vet command.." >&2
+  local targets=$(go list ./... 2>/dev/null | grep -v /vendor/)
+  if [[ ! "$targets" ]]; then
+    return 0 # Nothing to test
+  fi
+  local output
+  output=$(go vet $targets 2>&1)
+  if [ $? -eq 0 ]; then
+    return 0
+  fi
+  if echo "$output" | grep "matched no packages" >/dev/null; then
+    # Special case for "there's no packages in this repo", which is fine.
+    return 0
+  fi
+  echo "Go vet failed:\n$output" >&2
+  return 1
 }
 
-function update_bindata {
-	if [ -d "bindata/" ]; then
-		echo "Checking if bindata needs regenerating.." >&2
-		go-bindata -pkg="bindata" -o bindata/bin.go tmpl/
-		# TODO: since go-bindata doesn't properly format its output, we need
-		# to do it ourselves.
-		gofmt -w bindata/bin.go
-		dirty=$(prevent_dirty_tree bindata/ "commit bindata changes first")
-		return ${dirty}
-	fi
-	return 0
+update_bindata() {
+  if [ -d "bindata/" ]; then
+    echo "Checking if bindata needs regenerating.." >&2
+    go-bindata -pkg="bindata" -o bindata/bin.go tmpl/
+    # Note: since go-bindata doesn't properly format its output, we need
+    # to do it ourselves.
+    gofmt -w bindata/bin.go
+    local dirty=$(prevent_dirty_tree bindata/ "commit bindata changes first")
+    return ${dirty}
+  fi
+  return 0
 }
 
-function update_godep {
-	if [ -d "Godeps/" ]; then
-		echo "Checking for godeps updates.." >&2
-		godep update ...
-		dirty=$(prevent_dirty_tree Godeps/ "commit dependency changes first")
-		return ${dirty}
-	fi
-	return 0
+update_godep() {
+  if [[ -d "Godeps/" ]]; then
+    echo "Checking for godeps updates.." >&2
+    godep update ...
+    local dirty=$(prevent_dirty_tree Godeps/ "commit dependency changes first")
+    return ${dirty}
+  fi
+  return 0
 }
